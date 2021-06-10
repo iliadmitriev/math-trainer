870ca33be57fbadef8a4a396166c4982e8801a0f
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-06-10 15:15:36 +0300

    find question and progress bar after clicking a button

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,11 +1,21 @@
 import {mount} from '@vue/test-utils'
 import App from "../../src/App.vue";
+import StartScreen from "../../src/components/StartScreen.vue";
+import ProgressBar from "../../src/components/ProgressBar.vue";
 
 describe('App', () => {
   const wrapper = mount(App)
 
   it('render root application', () => {
     expect(wrapper.html()).toContain(`Вы готовы начать?`)
+  })
+
+  it('find component start screen', () => {
+    expect(wrapper.findComponent(StartScreen).exists()).toBe(true)
+  })
+
+  it('find component progress bar', () => {
+    expect(wrapper.findComponent(ProgressBar).exists()).toBe(true)
   })
 
 })
