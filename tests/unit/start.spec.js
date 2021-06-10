c8f0d3280e12aac39858221c9a2bfc83a55c3345
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-06-10 15:15:11 +0300

    start screen find question after clicking a button

--- tests/unit/start.spec.js
+++ tests/unit/start.spec.js
@@ -1,5 +1,6 @@
 import {mount} from '@vue/test-utils'
 import App from "../../src/App.vue";
+import Question from "../../src/components/Question.vue";
 
 describe('App', () => {
   const wrapper = mount(App)
@@ -7,6 +8,8 @@ describe('App', () => {
   test('push start button on start screen', async () => {
     const button = wrapper.find('#btnStart')
     await button.trigger('click')
+
+    expect(wrapper.findComponent(Question).exists()).toBe(true)
 
   })
 
